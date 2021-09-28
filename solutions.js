5c6a08bc71fe33a386a24400484e679e69ba3099
Makayla Clausen <makayla.clausen@bit.ly>
2021-09-28 09:31:09 -0600

    fix formatting issues

--- solutions.js
+++ solutions.js
@@ -1,20 +1,20 @@
 // Project Euler - Problem #2:
 const getFibSum = () => {
   let result = 0;
-	let previous = 0;
-	let current = 1;
+  let previous = 0;
+  let current = 1;
 
-	for (let i = 0; ; i++) {
-		let next = previous + current;
-		if (next > 4000000){
-			break;
-		}
-		if (next % 2 === 0){
-			result += next
-		}
-		previous = current
-		current = next 
-	}
-	console.log('Problem 2', result)
-}
-getFibSum()
+  for (let i = 0; ; i++) {
+    let next = previous + current;
+    if (next > 4000000) {
+      break;
+    }
+    if (next % 2 === 0) {
+      result += next;
+    }
+    previous = current;
+    current = next;
+  }
+  console.log("Problem 2", result);
+};
+getFibSum();
